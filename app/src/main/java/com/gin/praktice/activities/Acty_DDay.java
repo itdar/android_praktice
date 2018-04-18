54f873072d15b08dc5fb69ebdc5d1efb9db39020
jin <thesockerr@gmail.com>
2018-04-19 00:10:28 +0900

    Tested connection with contacts.

--- app/src/main/java/com/gin/praktice/activities/Acty_DDay.java
+++ app/src/main/java/com/gin/praktice/activities/Acty_DDay.java
@@ -2,12 +2,19 @@ package com.gin.praktice.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.Toast;
 
 import com.gin.praktice.R;
 
 public class Acty_DDay extends Activity {
+    private static final int PICK_CONTACT_REQUEST = 1;
+
+    private static final int PICK_CONTACT = 0;
+
     private Intent locationIntent;
 
     @Override
@@ -20,8 +27,41 @@ public class Acty_DDay extends Activity {
 
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.addFromPhoneButton : openPhoneBook(); break;
+            case R.id.addFromKKOButton : /*startActivity(locationIntent)*/; break;
             case R.id.nextButton : startActivity(locationIntent); break;
             default: break;
+        }
+    }
+
+    private void openPhoneBook() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            Cursor cursor = getContentResolver().query(data.getData(),
+                    new String[] { ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                            ContactsContract.CommonDataKinds.Phone.NUMBER },
+                    null, null, null);
+
+            cursor.moveToFirst();
+            //이름획득
+            String receiveName = cursor.getString(0);
+            //전화번호 획득
+            String receivePhone = cursor.getString(1);
+            cursor.close();
+
+            Toast.makeText(this, "연락처 이름 : " + receiveName + "\n연락처 전화번호 : " + receivePhone, Toast.LENGTH_LONG).show();
         }
     }
 }
