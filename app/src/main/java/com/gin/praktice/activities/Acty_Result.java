7083666e9acf4f81be1d0067a5b828d7978ef661
itdar <thesockerrr@gmail.com>
2018-06-25 23:37:23 +0900

    Fixed resultActy gui for managerFunction.

--- app/src/main/java/com/gin/praktice/activities/Acty_Result.java
+++ app/src/main/java/com/gin/praktice/activities/Acty_Result.java
@@ -16,6 +16,7 @@ public class Acty_Result extends Activity {
     private DDay dDay;
 
     private TextView resultTextView;
+    private TextView resultTextView2;
 
     // activity 정리하고 꾸며야함
     @Override
@@ -25,19 +26,21 @@ public class Acty_Result extends Activity {
 
         dDay = DDay.getInstance();
         resultTextView = (TextView) findViewById(R.id.resultTextView);
+        resultTextView2 = (TextView) findViewById(R.id.resultTextView);
 
         Visitor editTextVisitor = new Visitor_AppendEditText();
         dDay.accept(editTextVisitor, resultTextView);
         dDay.dayMembers.accept(editTextVisitor, resultTextView);
 
-        //새로 TextView 만들어서 각 매니저한테 얼마씩 줘야 하는지 출력 되도록 해야함
         //만들때 visitor 새로 만들어야함
     }
 
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.doneButton : doneButtonClicked(); break;
+            case R.id.saveButton : saveButtonClicked(); break;
             case R.id.copyResultButton : copyResultButtonClicked(); break;
+            case R.id.copyResultButton2 : copyResultButton2Clicked(); break;
             default: break;
         }
     }
@@ -61,8 +64,20 @@ public class Acty_Result extends Activity {
         finish();
     }
 
+    private void saveButtonClicked() {
+
+    }
+
+    private void copyResultButton2Clicked() {
+
+    }
+
     /**
-     * 마지막에 DDay->Location 돌면서 Manager따로 빼두고(Member), 각 Location의 멤버들을 빼둔 Manager정보랑 같이 각각 출력
+     * 마지막에 DDay->Location 돌면서
+     *  각 차마다 Manager따로 빼두고(Member), 나중에 계좌번호 등 필요해서 멤버객체로
+     *  각 Location의 멤버들을 빼둔 Manager 에다가 이름하고 각 금액 더해준다
+     * 다 돌고 나면 -> ManagerMembers 에서 ManagerMember 객체들에 돈 받아낼 이름, 금액 적인거 갖고 있음
+     * 출력한다
      */
 
 }
